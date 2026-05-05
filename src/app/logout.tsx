0f44ad172f795976470840d83cb39b311124966f
aV3RMA <alwaysaditya.av@gmail.com>
2026-05-05 10:39:34 +0530

    added authtication to the website

---
+++ src/app/logout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@base-ui/react";
+import { useRouter } from "next/navigation";
+
+export const LogoutButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/login");
+            },
+          },
+        })
+      }
+    >
+      Logout
+    </Button>
+  );
+};
